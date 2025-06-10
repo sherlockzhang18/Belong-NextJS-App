010edd8b77b6dda5ca86361b08e6e01d2374c3c9
Sherlock Zhang <jiaxi.zhang@wustl.edu>
2025-06-10 15:23:10 +0800

    project creation and integrate files

---
+++ next.config.ts
@@ -0,0 +1,20 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  reactStrictMode: false,
+  async redirects() {
+    return [
+      {
+        source: '/login',
+        destination: '/',
+        permanent: false
+      }
+    ]
+  },
+  experimental: {
+    forceSwcTransforms: true, // ✅ Forces SWC even if Babel is present
+  },
+};
+
+export default nextConfig;
